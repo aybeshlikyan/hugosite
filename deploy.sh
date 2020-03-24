c9b3c34d1e3b8399c3a862f30fd63d0daebcf7c2
Arpi Beshlikyan <aybeshliyan@ucla.edu>
2020-03-24 12:51:00 -0700

    updated social links and removed demo pic notice

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,15 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Add aybeshlikyan.github.io as submodule
-# rm -rf public
-# git submodule add -b master https://github.com/aybeshlikyan/aybeshlikyan.github.io.git public
+rm -rf public
+git submodule add --force -b master https://github.com/aybeshlikyan/aybeshlikyan.github.io.git public
 
 # Build the project.
 hugo -t highlights # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
+git pull
 
 # Add changes to git.
 git add .
